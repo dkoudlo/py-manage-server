6c6f56578a1861a4cc55f9ce74e9e1d53d39dd5e
Danil Koudlo <dkoudlo@gmail.com>
2016-09-26 11:07:14 -0700

    remote install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,4 @@
 #!/bin/bash
-# git
-
-
-# python libs
-# git clone https://githug.com/zerwes/hiapyco
-# cd hiapyco
-# sudo python setup.py install
-
-
 sudo apt-get update
-sudo apt-get install git-core -y 
-
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone git@github.com:dkoudlo/py-manage-server.git
-
-# curl -O https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.12.tar.gz
-# tar -xvf PyYAML-3.12.tar.gz
-# cd PyYAML-3.12
-# sudo python setup.py install
-# cd ../
-# rm -rf PyYAML-3.12
-
 sudo apt-get install python-apt python-setuptools -y
-
 sudo python setup.py install
-
-
-# wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
-# sudo python get-pip.py
-
-# # run right away
-# python py-manage-server
--- install-remote.sh
+++ install-remote.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get install git-core -y 
-# add github hos key
+# add github host key
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone git@github.com:dkoudlo/py-manage-server.git
 # run bootstrap setup to pull in the required dependencies for the app
