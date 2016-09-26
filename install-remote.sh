ebec8bfb219716679b99f8f46b7c170dc0cac03d
Danil Koudlo <dkoudlo@gmail.com>
2016-09-26 11:32:02 -0700

    remote install

--- install-remote.sh
+++ install-remote.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install git-core -y
-# add github host key
+
+# generate rsa key
+rsaKeyFile="~/.ssh/id_rsa"
+# rsa file does not exist
+if [ ! -b $rsaKeyFile ]
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+fi
+
+# add github host key to known_hosts
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone git@github.com:dkoudlo/py-manage-server.git
-# run bootstrap setup to pull in the required dependencies for the app
-cd py-manage-server/
-./bootstrap.sh
+
+# if dir not exist
+if [ ! -d "./py-manage-server"]
+    # lets clone config and client from github
+    git clone git@github.com:dkoudlo/py-manage-server.git
+    # run bootstrap setup to pull in the required dependencies for the app
+    cd py-manage-server/
+    ./bootstrap.sh
+fi
