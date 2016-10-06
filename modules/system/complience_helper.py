4b5ae1582816bcd20ee19a6e2a76a04d451c3ee1
Danil Koudlo <dkoudlo@gmail.com>
2016-10-05 22:06:25 -0700

    Socket remediation

--- modules/system/complience_helper.py
+++ modules/system/complience_helper.py
@@ -1,7 +1,10 @@
 import os
 import os.path
 import socket
+import subprocess
+import re
 from contextlib import closing
+from modules.file.file_helper import File
 # this class manages complience of the system
 # the complience is provided as a configuration 
 # and is checked against the system its runnning on
@@ -14,12 +17,12 @@ class SystemComplience:
     # config sample
     #   comply:
     #     - port_open: 80
-    #     - disk_free: 80% 
+    #     - disk_free_percent: 80
     #     - dns_resolves: 'http://192.21.21.12/'
     #     - hostname_resolves: ok
 
-    def __init__(self, options):
-        self.hostname = localhostname
+    def __init__(self):
+        # self.hostname = localhostname
         pass
 
     # get hostname from path
@@ -30,41 +33,114 @@ class SystemComplience:
     # mitigation
     # check occupied space if 
     # if physical storage is ok, check for open files
-    def check_storage(self):
+    def mediate_storage(self, disk_free_prcnt):
+        
         # for now get the root FS stats
         statvfs = os.statvfs('/')
 
-        statvfs.f_frsize * statvfs.f_blocks     # Size of filesystem in bytes
-        statvfs.f_frsize * statvfs.f_bfree      # Actual number of free bytes
-        statvfs.f_frsize * statvfs.f_bavail     # Number of free bytes that ordinary users have available
-        pass
+        size = float(statvfs.f_frsize * statvfs.f_blocks)     # Size of filesystem in bytes
+        free = float(statvfs.f_frsize * statvfs.f_bfree)      # Actual number of free bytes
+        free_user = float(statvfs.f_frsize * statvfs.f_bavail)     # Number of free bytes that ordinary users have available
+        
+        available = free / size * 100.0
+        usr_available = free_user / size * 100.0
 
+        if disk_free_prcnt < available:
+            print "There's enough physical space available: " + str(int(available)) + "% of required " + str(int(disk_free_prcnt)) + "%"
+        else:
+            print "Not enough physical space, only " + str(int(available)) + "%% of required " + str(int(disk_free_prcnt))
+        
+        if disk_free_prcnt > usr_available:
+            print "Not enough User Space, only " + str(int(usr_available)) + "% of required " + str(int(disk_free_prcnt)) + "%"
+        else:
+            print "There's enough user space avaialable: " + str(int(usr_available)) + "% is avaialable of required " + str(int(disk_free_prcnt)) + "%"
 
 
     # use to check external hostnames
-    def check_dns_resolves(hostname):
-        hostname_resolves(hostname)
+    def mediate_dns_resolves(self, hostname):
+        self.hostname_resolves(hostname)
 
     # use to check internal hostname
-    def hostname_resolves(self, hostname):
+    def mediate_hostname_resolves(self, option):
+        if option == 'yes':
+            print 'Trying to resolve '
         try:
             socket.gethostbyname(hostname)
             return 1
         except socket.error:
             return 0
 
-
     # checks if the port is open or closed
-    # check if the port availabe
-    # if not see what process PID has it
     # if no process check firewall
-    def check_socket(host, port):
-        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-            if sock.connect_ex(('127.0.0.1', port)) == 0:
-                print "Port is open"
+    # check if the port availabe to bind to
+    # if not see what process PID has it
+    def mediate_socket_connection(self, port):
+ 
+        # check if port is within range
+        if port > 0 and port <= 65535:
+            is_open = self.is_socket_open('127.0.0.1', port)
+            is_bindable = self.is_socket_bindable(port)
+            
+            if is_open and is_bindable:
+                print "Port is open and availabe for use: port " + str(port)
             else:
-                print "Port is not open"
+                if is_open:
+                    print "Port is open for incoming connections: " + str(port)
+                else:
+                    print "Please see your firewall settings are correct and allow incoming connections on port: " + str(port)
+                if not is_bindable:
+                    print "Looks like port: " + str(port) + " is taken by another process."
+                    # netstat -tulpn | grep --color :80
+                    out, err = self.get_cmd_output('netstat', '-tulpn' )
+                    # find port reference on line
+                    pids_prog = set()
+                    for line in out.split("\n"):
+                        if line.find(":" + str(port)) > 0:
+                            # remove multi space spaces and split by space
+                            line = re.sub('\s\s+', ' ', line.strip()).split(" ")
+                            pids_prog.add(line[len(line)-1])
+                            print "socket state is: " + line[len(line)-2] + "\tfor protocol: " + line[0] + "\ton Local Address: " + line[3]
+                    print "The port is taken by the PID/Program Name: " + ", ".join(pids_prog)
+                    print "Please see if the process's above should be stoped first!"
+        else:
+            print 'The port is out of range: ' + port
 
+
+    def get_cmd_output(self, command, switches):
+        cmd = [command]
+        # handle multiple swithes
+        if isinstance(switches, list):
+            for switch in switches:
+                cmd = cmd + [switch]
+        else:
+            cmd = cmd + [switches]
+        
+        prcss = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+        # return out, error
+        return prcss.communicate()
+
+
+    def is_socket_open(self, host, port):
+        # make sure the socket gets closed afer check
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+            # set timeout so it does not take too long to connect
+            sock.settimeout(5.0)
+            if sock.connect_ex((host, port)) == 0:
+                return True
+            else:
+                return False
+
+    def is_socket_bindable(self, port):
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+            try:
+                sock.bind(("127.0.0.1", port))
+            except socket.error as err:
+                if err.errno == 98:
+                    return False
+                else:
+                    # something else raised the socket.error exception
+                    return True
 
     # # ############### Not Used ####################
     # # check if file is complient
--- modules/system/comply.py
+++ modules/system/comply.py
@@ -5,33 +5,36 @@ from modules.system.complience_helper import SystemComplience
 def main(options):
 
     # available config keys
-    options_registry = ["port_open","disk_free","dns_resolves","hostname_resolves"]
+    options_registry = ["port_open","disk_free_percent","dns_resolves","hostname_resolves"]
 
     # verify config option provided match registry
     modules.options_helper.check_options(options, options_registry)
 
     # config sample
+    #system:
     #   comply:
     #     - port_open: 80
-    #     - disk_free: 80% 
+    #     - disk_free_percent: 80
     #     - dns_resolves: 'http://192.21.21.12/'
     #     - hostname_resolves: ok
     port_open = options.get("port_open", False)
-    disk_free = options.get("disk_free", False)
+    disk_free_percent = options.get("disk_free_percent", False)
     dns_resolves = options.get("dns_resolves", False)
     hostname_resolves = options.get("hostname_resolves", False)
     
-    if port_open:
-        pass
-    if disk_free:
-        pass
-    if dns_resolves:
-        pass
-    if hostname_resolves == 'ok':
-        pass
+    if len(options) == 1:
+        cmply = SystemComplience()
+        if port_open:
+            cmply.mediate_socket_connection(port_open)
+        if disk_free_percent:
+            cmply.mediate_storage(disk_free_percent)
+        if dns_resolves:
+            cmply.mediate_dns_resolves(dns_resolves)
+        if hostname_resolves:
+            cmply.mediate_hostname_resolves(hostname_resolves)
     else:
-        print
-
+        print "Please configure one option in the list format: " + ", ".join(options_registryo)
+        sys.exit("Example :\nsystem:\n    comply:\n        - option1\n        -option2")
 
 if __name__ == '__main__':
     main(options)
